563c3fa55d7de31dcba57d252209638cd9aaa474
zahdourmoustafa <m.zahdour@esi-sba.dz>
2025-05-22 14:50:50 +0100

    ch44

--- src/components/ClientForm.tsx
+++ src/components/ClientForm.tsx
@@ -124,7 +124,7 @@ const ClientForm = ({ client, onSuccess }: ClientFormProps) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-4">
+      <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-4 overflow-y-auto ">
         <FormField
           control={form.control}
           name="full_name"
